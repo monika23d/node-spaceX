cc61fa1fabc0be29e97353ccc8d53d515248888c
monika23d <monika23.deshmukh@gmail.com>
2021-02-07 13:49:40 +0530

    updated for cleanup changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 var express = require( "express" );
 var apiRouter = require('./api');
+const cors = require('cors');
 
 module.exports = () => {
     const app = express()
+    app.use(cors())
     app.use('/api',apiRouter)
     return app
 }
--- src/service/launch.service.js
+++ src/service/launch.service.js
@@ -16,12 +16,30 @@ const launchService = {
             
             request(options,(error,restResp, body) => {
                 if(!error){
-                    resolve(body)
+                    let resposnse = this.composeResponse (body)
+                    resolve(JSON.stringify(resposnse))
                 } else {
                     reject(error)
                 }
             })
         })
+    },
+
+    composeResponse(details){
+        let launchDetailsArr = []
+        details.forEach(elm => {
+            let launch = {
+                missionName:elm.mission_name,
+                flightNum: elm.flight_number,
+                missionID: elm.mission_id,
+                launchYear: elm.launch_year,
+                successLaunch:elm.launch_success ? elm.launch_success : false,
+                rocketDetails:elm.rocket.first_stage && elm.rocket.first_stage.cores,
+                imgLink:elm.links && elm.links.mission_patch_small
+            }
+            launchDetailsArr.push(launch)
+        });
+        return launchDetailsArr
     }
 }
 

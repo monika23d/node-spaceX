386255b460b32c28cbfdd62ebc4dfa7ce8364f8c
monika23d <monika23.deshmukh@gmail.com>
2021-02-07 14:36:49 +0530

    filter functionality

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 
 module.exports = () => {
     const app = express()
-    app.use(cors())
+    app.use(cors()) // TO remove CORS error while calling directly from UI
     app.use('/api',apiRouter)
     return app
 }

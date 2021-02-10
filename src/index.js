27511a7c6c92eef0c7c30b35e5610e5f93922edd
monika23d <monika23.deshmukh@gmail.com>
2021-02-10 11:00:14 +0530

    updated for build

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 var express = require( "express" );
 var apiRouter = require('./api');
+const apiRoute = "/.netlify/functions/server/api";
 const cors = require('cors');
+const router = express.Router();
+const serverless = require("serverless-http");
 
-module.exports = () => {
+//module.exports = () => {
     const app = express()
     app.use(cors()) // TO remove CORS error while calling directly from UI
-    app.use('/api',apiRouter)
-    return app
-}
+    app.use(apiRouter,router)
+    // return app
+//}
+
+module.exports = app
+module.exports.handler = serverless(app);
+
 
 
